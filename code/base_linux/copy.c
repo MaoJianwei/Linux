454e1386a1d5c294592d4b02b97521c4f2f39979
yjjieqc <lonelypiscesve@gmail.com>
2018-05-08 22:13:38 +0800

    modify copy

--- code/base_linux/copy.c
+++ code/base_linux/copy.c
@@ -7,6 +7,11 @@
 int main(void){
 	int n;
 	char buf[BUFFSIZE];
-	while((n= read(STDIN_FILENO, buf, BUFFSIZE)) > 0)
-		if(write(STDOUT_FILENO
+	while((n= read(STDIN_FILENO, buf, BUFFSIZE)) > 0){
+		if(write(STDOUT_FILENO, buf, n) != n)
+			err_sys("write error");
+	}
+	if(n < 0)
+		err_sys("read error");
+	exit(0);
 }
